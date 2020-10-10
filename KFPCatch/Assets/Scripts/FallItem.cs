31ae0b6e51648dea257d03a0c1b9b560b9e3c4f1
DESKTOP-2HR41AL\Lua <luavtuber@gmail.com>
2020-10-10 14:23:01 -0700

    Started prefab setup

---
+++ KFPCatch/Assets/Scripts/FallItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class FallItem : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -6)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
