b4f1e396df9bbe02b5323309eaa660c13e2d0e8f
DESKTOP-2HR41AL\Lua <luavtuber@gmail.com>
2020-10-10 21:09:37 -0700

    Title Card setup and basic prefab setup

---
+++ KFPCatch/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce(movement * speed);
+    }
+}
---
+++ KFPCatch/Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+
+{
+
+    public bool Active;
+
+    public GameObject Item;
+    public GameObject Bomb;
+
+    public float SpawnRate;
+    public float BombPercent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Active &&  Random.value <= SpawnRate)
+        {
+            // regular chicken spawn
+            if(Random.value >= BombPercent)
+            {
+                GameObject obj = Instantiate(Item);
+            }
+            // bad chicken spawn
+            else
+            {
+                GameObject obj = Instantiate(Bomb);
+            }
+        }
+    }
+}
